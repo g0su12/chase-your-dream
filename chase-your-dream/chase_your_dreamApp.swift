0597856f704ed16725a8990740b309526ea24bde
huynq1 <huynq1@hblab.vn>
2026-04-24 10:10:59 +0700

    Initial Commit

---
+++ chase-your-dream/chase_your_dreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  chase_your_dreamApp.swift
+//  chase-your-dream
+//
+//  Created by HuyNQ1 on 24/4/26.
+//
+
+import SwiftUI
+
+@main
+struct chase_your_dreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
